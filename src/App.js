6f9eca454a516125f2265f9baf262be043469646
luistope98 <33384695+luistope98@users.noreply.github.com>
2019-11-13 19:48:42 -0500

    prueba2

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn 
+          dylan las mejores
         </a>
       </header>
     </div>
